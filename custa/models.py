c92be4fb8bb5446ea6e964705a1d298856a33f51
Radu Petrescu <2411100P@student.gla.ac.uk>
2019-03-06 13:27:27 +0000

    models complete and working

--- custa/models.py
+++ custa/models.py
@@ -5,41 +5,40 @@ from django.contrib.auth.models import User
 char_field_max_length = 128
 
 
-class Base(models.Model):
-    # id primary key is by default
-    base = models.CharField(max_length=char_field_max_length, unique=True) # Unique base.
-    price = models.IntegerField(default=0) # Remember price is in pence henceforth!
-
-    def __str__(self):
-        return self.base
-
-
 class Sauce(models.Model):
     # id primary key is by default
-    sauce = models.CharField(max_length=char_field_max_length, unique=True)  # Unique sauce.
+    name = models.CharField(max_length=char_field_max_length, unique=True)  # Unique sauce.
     price = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.sauce
+        return self.name
 
 
 class Top(models.Model):
     # id primary key is by default
-    top = models.CharField(max_length=char_field_max_length, unique=True)  # Unique top.
+    name = models.CharField(max_length=char_field_max_length, unique=True)  # Unique top.
     price = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.top
+        return self.name
+
+class Base(models.Model):
+    # id primary key is by default
+    name = models.CharField(max_length=char_field_max_length, unique=True)  # Unique base.
+    price = models.IntegerField(default=0)  # Remember price is in pence henceforth!
+
+    def __str__(self):
+        return self.name
 
 
 class Custa(models.Model):
     # id primary key is by default
     name = models.CharField(max_length=char_field_max_length, unique=True)
     price = models.IntegerField(default=0)
-    base_id = models.OneToOneField(Base, on_delete=models.CASCADE)
-    sauce_id = models.OneToOneField(Sauce, on_delete=models.CASCADE)
-    top_id = models.OneToOneField(Top, on_delete=models.CASCADE)
     username = models.ForeignKey(User, on_delete=models.CASCADE)
+    base_name = models.OneToOneField(Base, on_delete=models.CASCADE)
+    sauce_name = models.OneToOneField(Sauce, on_delete=models.CASCADE)
+    top_name = models.OneToOneField(Top, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -50,8 +49,8 @@ class UserProfile(models.Model):
     # id primary key is by default
     user = models.OneToOneField(User)
     pref_name = models.CharField(max_length=char_field_max_length, null=True)  # Not necessary to have a preferred name.
-    phone = models.IntegerField(null=False) # Must provide a phone number
-    address = models.CharField(max_length=char_field_max_length, null=False) # Must provide an address.
+    phone = models.IntegerField(null=False)  # Must provide a phone number
+    address = models.CharField(max_length=char_field_max_length, null=False)  # Must provide an address.
 
     def __str__(self):
         return self.user.username
@@ -59,9 +58,6 @@ class UserProfile(models.Model):
 
 class Order(models.Model):
     # id primary key is by default
-    date = models.DateField(null=False) # Must provide Date Field.
-    order_details = models.CharField(max_length=char_field_max_length, null=False) # Must provide order details.
+    date = models.DateField(null=False)  # Must provide Date Field.
+    order_details = models.CharField(max_length=char_field_max_length, null=False)  # Must provide order details.
     username = models.ForeignKey(User, on_delete=models.CASCADE)
-
-
-
--- populate_custa.py
+++ populate_custa.py
@@ -13,40 +13,40 @@ from custa.models import Base, Sauce, Top
 
 def populate():
     bases = [
-        {"base": "Macaroni", "price": 100}, ]
+        {"name": "Macaroni", "price": 100}, ]
 
     for b in bases:
-        add_base(b["base"], b["price"])
-        print(b["base"] + b["price"])
+        add_base(b["name"], b["price"])
+        print(b["name"] + " " + str(b["price"]))
 
     sauces = [
-        {"sauce": "Macaroni Cheese", "price": 100, }, ]
+        {"name": "Macaroni Cheese", "price": 100, }, ]
 
     for s in sauces:
-        add_sauce(s["sauce"], s["price"])
-        print(s["sauce"] + s["price"])
+        add_sauce(s["name"], s["price"])
+        print(s["name"] + " " + str(s["price"]))
 
-    tops = [{"top": "Parmigiano Reggiano", "price": 50, }, ]
+    tops = [{"name": "Parmigiano Reggiano", "price": 50, }, ]
 
     for t in tops:
-        add_top(t["top"], t["price"])
-        print(t["top"] + t["price"])
+        add_top(t["name"], t["price"])
+        print(t["name"] + " " + str(t["price"]))
 
 
 def add_base(base, price=0):
-    b = Base.objects.get_or_create(base=base, price=price)[0]
+    b = Base.objects.get_or_create(name=base, price=price)[0]
     b.save()
     return b
 
 
 def add_sauce(sauce, price=0):
-    s = Sauce.objects.get_or_create(sauce=sauce, price=price)[0]
+    s = Sauce.objects.get_or_create(name=sauce, price=price)[0]
     s.save()
     return s
 
 
 def add_top(top, price=0):
-    t = Top.objects.get_or_create(top=top, price=price)[0]
+    t = Top.objects.get_or_create(name=top, price=price)[0]
     t.save()
     return t
 
